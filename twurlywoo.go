b0eeba02cfe5267d90940f11b21fd2022e42a0a4
David Thompson <dtt101@gmail.com>
2017-08-03 22:53:33 +0100

    Get URLS from quoted tweets and retweets
    Ignore any URL’s pointing to things hosted on twitter.com (images and
    other statuses)

--- twurlywoo.go
+++ twurlywoo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/subosito/gotenv"
@@ -18,6 +19,19 @@ func getClient() *anaconda.TwitterApi {
 	return api
 }
 
+func getTweetUrls(tweet *anaconda.Tweet) string {
+	tweets := ""
+	for _, url := range tweet.Entities.Urls {
+		if url.Expanded_url != "" {
+			if !strings.HasPrefix(url.Expanded_url, "https://twitter.com") {
+				tweets += fmt.Sprintf("<a href=\"%s\">%[1]s</a>\n", url.Expanded_url)
+				tweets += "<br>"
+			}
+		}
+	}
+	return tweets
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	api := getClient()
@@ -27,11 +41,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	for _, tweet := range homeResult {
-		for _, url := range tweet.Entities.Urls {
-			if url.Expanded_url != "" {
-				fmt.Fprintf(w, "<a href=\"%s\">%[1]s</a>\n", url.Expanded_url)
-				fmt.Fprintln(w, "<br>")
-			}
+		fmt.Fprint(w, getTweetUrls(&tweet))
+		if tweet.QuotedStatus != nil {
+			fmt.Fprint(w, getTweetUrls(tweet.QuotedStatus))
+		} else if tweet.RetweetedStatus != nil {
+			fmt.Fprint(w, getTweetUrls(tweet.RetweetedStatus))
 		}
 	}
 }
